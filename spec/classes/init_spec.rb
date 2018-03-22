acfc6dd750c5140c700f7e57c1eb03f0154d1a6d
Kevin Coakley <kcoakley@sdsc.edu>
2018-03-22 10:55:18 -0700

    Refactored to pass rubocop tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,35 +1,27 @@
 require 'spec_helper'
 
 describe 'user' do
-
   it { is_expected.to compile }
   it { is_expected.to compile.with_all_deps }
 
   describe 'user::create' do
-
-    # it { should contain_user('kcoakley')}
-
-    it { is_expected.to contain_user('tester')
-      .with(
-        :ensure     => 'present',
-        :comment    => 'Test User',
-        :home       => '/home/tester',
-        :managehome => true,
-        :uid        => 2018,
-
+    it do
+      is_expected.to contain_user('tester').with(
+        ensure: 'present',
+        comment: 'Test User',
+        home: '/home/tester',
+        managehome: true,
+        uid: 2018
       )
-    }
-
+    end
   end
 
   describe 'user::ssh_authorized_key' do
-
-    it { is_expected.to contain_ssh_authorized_key('tester')
-      .with(
-        :key  => 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==',
-        :type => 'ssh-rsa',
+    it do
+      is_expected.to contain_ssh_authorized_key('tester').with(
+        key: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==',
+        type: 'ssh-rsa'
       )
-    }
+    end
   end
-
 end
