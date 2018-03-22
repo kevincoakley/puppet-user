1977709aae3d518b85d4b1926535683eb75362a4
Kevin Coakley <kcoakley@sdsc.edu>
2018-03-22 09:43:22 -0700

    Added spec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,35 @@
 require 'spec_helper'
+
 describe 'user' do
-  context 'with default values for all parameters' do
-    it { should contain_class('user') }
+
+  it { is_expected.to compile }
+  it { is_expected.to compile.with_all_deps }
+
+  describe 'user::create' do
+
+    # it { should contain_user('kcoakley')}
+
+    it { is_expected.to contain_user('tester')
+      .with(
+        :ensure     => 'present',
+        :comment    => 'Test User',
+        :home       => '/home/tester',
+        :managehome => true,
+        :uid        => 2018,
+
+      )
+    }
+
   end
+
+  describe 'user::ssh_authorized_key' do
+
+    it { is_expected.to contain_ssh_authorized_key('tester')
+      .with(
+        :key  => 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==',
+        :type => 'ssh-rsa',
+      )
+    }
+  end
+
 end
