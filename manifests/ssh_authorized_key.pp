c0bc94a00ff7f0f35dda2ef1d1cd2663d647fb13
Kevin Coakley <kcoakley@sdsc.edu>
2018-03-22 12:41:01 -0700

    Updated documentation

--- manifests/ssh_authorized_key.pp
+++ manifests/ssh_authorized_key.pp
@@ -1,46 +1,13 @@
 # Class: user::ssh_authorized_key
 # ===========================
 #
-# Full description of class user here.
+# Adds SSH authorized keys using the ssh_authorized_key resource
 #
 # Parameters
 # ----------
 #
-# Document parameters here.
+# See https://puppet.com/docs/puppet/5.5/types/ssh_authorized_key.html
 #
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'user':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2018 Your name here, unless otherwise noted.
 #
 define user::ssh_authorized_key (
   $ensure   = undef,
